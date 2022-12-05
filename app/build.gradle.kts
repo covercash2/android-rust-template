d66fb12498e74a24717f087d4652d957f4b7fdec
Chris Overcash - c0o02bc <Chris.Overcash@walmart.com>
2022-12-04 23:58:22 -0600

    add cargo to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -62,3 +65,68 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
 }
+
+sealed class Abi {
+    abstract val android: String
+    abstract val rust: String
+
+    data class ArmV7(
+        override val android: String = "armeabi-v7a",
+        override val rust: String = "armv7-linux-androideabi",
+    ): Abi()
+
+    data class ArmV8(
+        override val android: String = "arm64-v8a",
+        override val rust: String = "aarch64-linux-android",
+    ): Abi()
+
+    companion object {
+        val All = listOf(ArmV7(), ArmV8())
+    }
+}
+
+val rustProjectDir = "../rust"
+val rustTargetDir = "$rustProjectDir/target"
+val targetJniDir = "src/main/jniLibs"
+
+Abi.All.forEach { abi ->
+    val cargoBuildTask = "cargoBuild-${abi.rust}"
+    task<Exec>(cargoBuildTask) {
+        description = "build static object for ${abi.rust}"
+
+        workingDir(rustProjectDir)
+        commandLine("cargo", "build", "--release", "--target", abi.rust)
+    }
+
+    val copyRustLibsTask = "copyRustLibs-${abi.android}"
+    val rustOutputDir = "$rustTargetDir/${abi.rust}/release"
+    val targetAbiDir = "$targetJniDir/${abi.android}"
+
+    task<Copy>(copyRustLibsTask) {
+        description = "copy compiled Rust binaries to the Android project"
+
+        dependsOn(cargoBuildTask)
+        from(rustOutputDir)
+        include("*.so")
+        into(targetAbiDir)
+    }
+
+    tasks.withType<JavaCompile> {
+        dependsOn(copyRustLibsTask)
+    }
+
+    val cleanJniLibsTask = "cleanJniLibs-${abi.android}"
+
+    task<Delete>(cleanJniLibsTask) {
+        delete(targetAbiDir)
+    }
+
+    tasks.named("clean").dependsOn(cleanJniLibsTask)
+}
+
+task<Exec>("cargoClean") {
+    workingDir(rustProjectDir)
+    commandLine("cargo", "clean")
+}
+
+tasks.named("clean").dependsOn("cargoClean")
